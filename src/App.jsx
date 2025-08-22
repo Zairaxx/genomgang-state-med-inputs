68dd9345fc6492b2a72ec23e3b7e6d0739ea0273
Zairaxx <brandontsegai@gmail.com>
2025-08-22 16:57:27 +0200

    cleaned up code

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ function App() {
 
       let updatedStudents = [...students];
       updatedStudents.push(studentName);
-    //Uppdatera state
       setStudents(updatedStudents)
 
     // //Alternativ 2
--- src/components/StudentForm.jsx
+++ src/components/StudentForm.jsx
@@ -4,15 +4,12 @@ const StudentForm = ({addStudent}) => {
 
     const [name,setName] = useState("");
 
-
     const handleNameInput = (event) => {
     setName(event.target.value);
   }
 
     return(
         <>
-            Nuvarande name state: {name}
-            <br></br>
             <input type="text" onChange={handleNameInput}/>
             <button onClick={() => {addStudent(name)}}>Lägg till</button>
         </>
